9a7b2ab8ed905c998ae8a3a298f68e216cc2a0aa
Aemal Sayer <a@aemalsayer.com>
2025-12-19 05:33:31 +0100

    added subtitile position

--- claude/main.js
+++ claude/main.js
@@ -158,8 +158,8 @@ PlayResY: 1080
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Arial,84,&H00FFFFFF,&H000000FF,&H00000000,&H80000000,1,0,0,0,100,100,0,0,1,4,4,5,50,50,50,1
-Style: Karaoke,Arial,84,&H0000FFFF,&H000000FF,&H00000000,&H80000000,1,0,0,0,100,100,0,0,1,4,4,5,50,50,50,1
+Style: Default,Arial,84,&H00FFFFFF,&H000000FF,&H00000000,&H80000000,1,0,0,0,100,100,0,0,1,4,4,2,50,50,${$('When clicking ‘Execute workflow’').first().json.subTitlePosition},1
+Style: Karaoke,Arial,84,&H0000FFFF,&H000000FF,&H00000000,&H80000000,1,0,0,0,100,100,0,0,1,4,4,2,50,50,${$('When clicking ‘Execute workflow’').first().json.subTitlePosition},1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
